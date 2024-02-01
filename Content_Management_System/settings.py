f11baefb1f330936544192da8be0a7ce5471f006
anas0student <148706460+anas0student@users.noreply.github.com>
2024-02-01 22:46:20 +0300

    Add files via upload

--- Content_Management_System/settings.py
+++ Content_Management_System/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'django-insecure-)rd(06930unhoubcp-i-nn21p6i!3u08wvyh4j5k@f&!_8)@8t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cmswep.anas.cryptid.foo', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://cmswep.anas.cryptid.foo']
 
 # Application definition
 
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "course_app",
+    # For Cors
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,9 +51,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
-ROOT_URLCONF = 'Content_Management_System.urls'
+CORS_ALLOWED_ORIGINS = [
+    "https://cmswep.anas.cryptid.foo",
+    # Add any other origins as needed
+]
 
 TEMPLATES = [
     {

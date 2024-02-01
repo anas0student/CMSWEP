3062d05f00f570d135854f4aae1074d36c857ab1
anas0student <148706460+anas0student@users.noreply.github.com>
2024-02-01 14:01:01 +0300

    Delete Content_Management_System/Content_Management_System directory

--- Content_Management_System/Content_Management_System/urls.py
+++
@@ -1,9 +0,0 @@
-# myproject/urls.py
-from django.contrib import admin
-from django.urls import include, path
-from course_app.views import home
-urlpatterns = [
-    path('admin/', admin.site.urls),
-     path('', home, name='home'),
-    path('course_app/', include('course_app.urls')),
-]

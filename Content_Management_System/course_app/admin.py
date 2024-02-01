3e1058090827d6c5f65811a8b4d188495994eb0e
anas0student <148706460+anas0student@users.noreply.github.com>
2024-02-01 14:01:12 +0300

    Delete Content_Management_System/course_app directory

--- Content_Management_System/course_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Course, Topic, CustomUser
-
-# Register your models here.
-admin.site.register(Course)
-admin.site.register(Topic)
-admin.site.register(CustomUser)
